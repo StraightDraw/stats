c69c1d8141f21ea18f068fb83391d8239d9382e7
StraightDraw <robbsinn@gmail.com>
2021-01-11 13:52:19 -0500

    updating file structure and toc.yml

---
+++ _build/jupyter_execute/Introduction.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Template for New JupyterBook
+
+# Rescource for developing your own content in a jupyterbook.
+
+# See the overview documentation at: [https://jupyterbook.org/intro.html](https://jupyterbook.org/intro.html).
+
+# 
+# ```{toctree}
+# :hidden:
+# :titlesonly:
+# 
+# 
+# ch1/0
+# ch2/0
+# ```
+# 
