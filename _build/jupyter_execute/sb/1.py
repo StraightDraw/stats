a2ab0b4853707752114c2b6bf143e6a8671fe499
StraightDraw <robbsinn@gmail.com>
2021-02-17 05:02:54 -0500

    updated 1.1 and 1.4

--- _build/jupyter_execute/sb/1.py
+++ _build/jupyter_execute/sb/1.py
@@ -18,19 +18,24 @@ pers
 
 narc = pers.select('Sex','Narc')
 
-narc
+The `nan` value indicates there is no value for that cell in the table. In this case, it's a survey item that went unanswered. The `numpy` function `nanmean` takes the average but ignores any `nan` values. In a clean table, we could just use `np.mean`, instead.
+
+narc.group('Sex', np.nanmean)
 
 integer_bins = np.arange(15)
 narc.hist('Narc', group = "Sex", bins = integer_bins)
-plots.title('Narcissism by Sex')
+_=plots.title('Narcissism by Sex')
 
 males_narcissism = narc.where('Sex',"M").column('Narc')
 
 females_narcissism = narc.where('Sex',"F").column('Narc')
 
-print('The average narcissism level for males is ', np.nanmean(males_narcissism),
+print('The average narcissism level for males is',
+      np.round(np.nanmean(males_narcissism),2),
       "\r\n",
-      'and the average narcissism level for females is ' , np.nanmean(females_narcissism))
+      'and the average narcissism level for females is', 
+      np.round(np.nanmean(females_narcissism),2)
+     )
 
 from scipy import stats
 
